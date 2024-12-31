e95a4e3de41872a4da3a1a9df8e628202a034790
Rohith Janwadkar <rohithjanwadkar@Benjamin-s-A11.guest.motel6.lan>
2024-12-31 02:33:16 -0600

    Added common entity services

---
+++ src/main/java/org/example/uberprojectentityservice/Models/Booking.java
@@ -0,0 +1,34 @@
+package org.example.uberprojectentityservice.Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking extends BaseModel{
+
+    @Enumerated(value = EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date startTime;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date endTime;
+
+    private Long totalDistance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Driver driver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Passenger passenger;
+
+}
---
+++ src/main/java/org/example/uberprojectentityservice/Models/BookingStatus.java
@@ -0,0 +1,10 @@
+package org.example.uberprojectentityservice.Models;
+
+public enum BookingStatus {
+    SCHEDULED,
+    CANCELLED,
+    CAB_ARRIVED,
+    ASSIGNING_DRIVER,
+    IN_RIDE,
+    COMPLETED
+}
